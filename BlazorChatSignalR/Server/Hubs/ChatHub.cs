e7153069a4a12d570602a4001bc9753cea502a8d
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-11-22 21:53:31 -0300

    build: add packages to signalr and configure dependences

---
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await SendMessageAsync("", "User connected!");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task SendMessageAsync(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
