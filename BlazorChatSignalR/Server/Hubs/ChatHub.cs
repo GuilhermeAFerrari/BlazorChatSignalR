cf7e5cbe5ae3770e59605c374970ee3f9aef6139
Guilherme Ferrari <guile.ferrari@hotmail.com>
2022-11-23 21:33:17 -0300

    feat: ending chat

--- BlazorChatSignalR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -4,10 +4,22 @@ namespace BlazorChatSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> _users = new Dictionary<string, string>();
+
         public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+
+            _users.Add(Context.ConnectionId, username);
+
+            await AddMessageToChat(string.Empty, $"{username} joined the chat!");
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = _users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left!");
         }
 
         public async Task AddMessageToChat(string user, string message)
